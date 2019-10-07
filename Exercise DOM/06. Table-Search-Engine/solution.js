ea7d5c57058c458b7d503748130624abf4ff0e28
Бойко Боев <55878188+BoykoPetevBoev@users.noreply.github.com>
2019-10-07 19:17:12 +0300

    Update solution.js

--- Exercise DOM/06. Table-Search-Engine/solution.js
+++ Exercise DOM/06. Table-Search-Engine/solution.js
@@ -1,3 +1,24 @@
 function solve() {
-   //TODO...
+   let input = document.getElementById('searchField');
+   let body = document.querySelectorAll('tbody tr');
+
+   if (input === null || body === null) {
+      throw new Error('Missing HTML elements!');
+   }
+
+   document.getElementById('searchBtn').addEventListener('click', main);
+   function main() {
+      for (let tr of body) {
+         tr.classList.remove('select');
+      }
+      if (input.value == '') {
+         return '';
+      }
+      for (let tr of body) {
+         if (tr.textContent.includes(input.value)) {
+            tr.classList.add('select');
+         }
+      }
+      input.value = '';
+   }
 }
