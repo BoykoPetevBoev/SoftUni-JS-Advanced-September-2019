f41cc00c14f4a2ce3cd4392e69ff8c0489295e6a
Бойко Боев <55878188+BoykoPetevBoev@users.noreply.github.com>
2019-10-10 14:23:57 +0300

    Update script.js

--- Exercise DOM Manipulations/04. Sections/script.js
+++ Exercise DOM Manipulations/04. Sections/script.js
@@ -1,3 +1,17 @@
 function create(words) {
-   console.log('TODO:...');
+   const display = document.getElementById('content');
+   for (let text of words) {
+      createElements(text)
+   }
+   display.addEventListener('click', function (el){
+       el.target.firstChild.style.display = 'block';
+   })
+   function createElements(text) {
+      const p = document.createElement('p');
+      const div = document.createElement('div');
+      p.textContent = text;
+      p.style.display = 'none';
+      div.appendChild(p);
+      display.appendChild(div);
+   }
 }
