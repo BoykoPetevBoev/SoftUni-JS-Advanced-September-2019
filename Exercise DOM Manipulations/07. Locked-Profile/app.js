14d7f0ebdf7088e60c92ac75c1e2be6c6c0c2c9a
Бойко Боев <55878188+BoykoPetevBoev@users.noreply.github.com>
2019-10-10 19:59:50 +0300

    Update app.js

--- Exercise DOM Manipulations/07. Locked-Profile/app.js
+++ Exercise DOM Manipulations/07. Locked-Profile/app.js
@@ -1,3 +1,27 @@
 function lockedProfile() {
-    console.log('TODO...')
+
+    const profiles = document.getElementsByClassName('profile')
+    Object.values(profiles).map(p => p.addEventListener('click', function (x) {
+        const elements = p.children;
+        if (elements[2] === x.target) {
+            lockAndUnlock(elements[2], elements[4]);
+        }
+        if (elements[4] === x.target) {
+            lockAndUnlock(elements[4], elements[2]);
+        }
+        if (elements[4].checked === true && x.target === elements[10]) {
+            if (elements[9].style.display === 'block') {
+                elements[9].style.display = 'none';
+                elements[10].textContent = 'Show more';
+            }
+            else {
+                elements[9].style.display = 'block';
+                elements[10].textContent = 'Hide it';
+            }
+        }
+        function lockAndUnlock(lock, unlock) {
+            lock.checked = true;
+            unlock.checked = false;
+        }
+    }));
 }
