dad0435fb38e2c4ecaf0c2c0a7d68038968a6990
Бойко Боев <55878188+BoykoPetevBoev@users.noreply.github.com>
2019-10-10 14:34:59 +0300

    Update scripts.js

--- Exercise DOM Manipulations/05. Notification/scripts.js
+++ Exercise DOM Manipulations/05. Notification/scripts.js
@@ -1,3 +1,6 @@
 function notify(message) {
-    console.log('TODO:...');
+    const div = document.getElementById('notification');
+    div.textContent = message;
+    div.style.display = 'block';
+    setTimeout(function(){ div.style.display = 'none';}, 2000);
 }
