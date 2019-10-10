c948ceacedade9b86740a684677a3d3f7597b85b
Бойко Боев <55878188+BoykoPetevBoev@users.noreply.github.com>
2019-10-10 15:19:45 +0300

    Update solution.js

--- Exercise DOM Manipulations/06. Time-Converter/solution.js
+++ Exercise DOM Manipulations/06. Time-Converter/solution.js
@@ -3,5 +3,36 @@ function attachEventsListeners() {
     const hoursInput = document.getElementById('hours');
     const minutesInput = document.getElementById('minutes');
     const secondsInput = document.getElementById('seconds');
-    document.getElementById('daysBtn').addEventListener('click', )
+
+    document.getElementById('daysBtn').addEventListener('click', function () {
+        let days = Number(daysInput.value);
+        convertDays(days);
+    })
+    document.getElementById('hoursBtn').addEventListener('click', function () {
+        let days = Number(hoursInput.value) / 24;
+        convertDays(days);
+    })
+    document.getElementById('minutesBtn').addEventListener('click', function () {
+        let days = Number(minutesInput.value) / 1440;
+        convertDays(days);
+    })
+    document.getElementById('secondsBtn').addEventListener('click', function () {
+        let days = Number(secondsInput.value) / 86400;
+        convertDays(days);
+    })
+    function convertDays(days) {
+        const convDays = {
+            'days': days,
+            'hours': days * 24,
+            'minutes': days * 1440,
+            'seconds': days * 86400
+        }
+        printResult(convDays);
+    }
+    function printResult(obj){
+        daysInput.value = obj['days'];
+        hoursInput.value = obj['hours'];
+        minutesInput.value = obj['minutes'];
+        secondsInput.value = obj['seconds'];
+    }
 }
