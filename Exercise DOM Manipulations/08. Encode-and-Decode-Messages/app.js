746f8043c56849b94bc75c87e649159f39ad900a
Бойко Боев <55878188+BoykoPetevBoev@users.noreply.github.com>
2019-10-10 21:54:08 +0300

    Update app.js

--- Exercise DOM Manipulations/08. Encode-and-Decode-Messages/app.js
+++ Exercise DOM Manipulations/08. Encode-and-Decode-Messages/app.js
@@ -1,3 +1,26 @@
 function encodeAndDecodeMessages() {
-    console.log('TODO...')
+    const buttons = document.getElementsByTagName('button');
+    const textArea = document.getElementsByTagName('textArea');
+
+    buttons[0].addEventListener('click', encodeMessage);
+    buttons[1].addEventListener('click', decodeMessage);
+
+    function encodeMessage() {
+        let operator = 1;
+        textArea[1].textContent = generateMessage(textArea[0].value, operator);
+        textArea[0].value = '';
+    }
+    function decodeMessage() {
+        let operator = (-1);
+        textArea[0].value = '';
+        textArea[1].textContent =  generateMessage(textArea[1].value, operator);
+    }
+    function generateMessage(message, operator) {
+        let newMessage = '';
+        for (let i = 0; i < message.length; i++) {
+            let char = message.charCodeAt(i);
+            newMessage += String.fromCharCode(char += operator);
+        }
+        return newMessage;
+    }
 }
