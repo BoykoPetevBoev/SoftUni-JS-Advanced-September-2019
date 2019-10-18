735bcade5b83bb6405071701b7ec77c9ef54f444
Бойко Боев <55878188+BoykoPetevBoev@users.noreply.github.com>
2019-10-18 18:43:57 +0300

    Add files via upload

---
+++ Exercise Unit Testing and Modules/06.Payment-Package-Tests.js
@@ -0,0 +1,10 @@
+const StringBuilder = require('./05.String-Builder.js');
+const expect = require('chai').expect;
+const assert = require('chai').assert;
+
+describe("TODO …", function() {
+    it("TODO …", function() {
+        // TODO: …
+    });
+    // TODO: …
+});
