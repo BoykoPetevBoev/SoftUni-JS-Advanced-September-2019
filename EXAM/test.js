dc83fb1acc9632a6e40101c0f9bcf2d17cde2507
Бойко Боев <boykopetevboev@gmail.com>
2019-12-09 12:30:05 +0200

    Create test.js

---
+++ EXAM/test.js
@@ -0,0 +1,14 @@
+let SkiResort = require('./solution');
+
+let expect = require('chai').expect;
+let assert = require('chai').assert;
+
+const beforeEach = require('mocha').beforeEach;
+
+describe('SkiResort', function () {
+    let obj;
+    beforeEach(function () {
+        obj = new PizzUni;
+    });
+    it("", function () {});
+});
